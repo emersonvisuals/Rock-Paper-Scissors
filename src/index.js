039e60501d30229d3b184215d79a584b789775c2
Thomas Emerson <emersonvisuals@gmail.com>
2022-09-27 18:41:00 +1000

    styling wip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('fgdfg');
+import './styles/main.scss'
+
+import iconPaperImport from './assets/icon-paper.svg';
+import iconScissorsImport from './assets/icon-scissors.svg';
+import iconRockImport from './assets/icon-rock.svg';
+
+
+const iconPaper = document.querySelector('.paper');
+iconPaper.src = iconPaperImport;
+
+const iconScissors = document.querySelector('.scissors');
+iconScissors.src = iconScissorsImport;
+
+const iconRock = document.querySelector('.rock');
+iconRock.src = iconRockImport;
